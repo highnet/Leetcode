c4882a4df2e6981954fa5944bfa1e33c0c23f405
joaquin telleria <joaquintelleria@gmail.com>
2019-11-18 15:11:53 +0100

    Update AvailableRookCaptures.cs

--- AvailableRookCaptures.cs
+++ AvailableRookCaptures.cs
@@ -1,102 +1,69 @@
-            public class Solution
+public class Solution {
+    public int NumRookCaptures(char[][] board) {
+        int rookY = 0;
+        int rookX = 0;
+        int hits = 0;
+        
+        int[] rookPos = FindRookXY(board);
+        rookX = rookPos[0];
+        rookY = rookPos[1];
+        
+        string[] directions = new string[]{"up","down","left","right"};
+        
+        foreach(string dir in directions){
+            hits += RayCast(dir,rookX,rookY,board)? 1:0;
+        }
+        
+        return hits;
+    }
+    
+    public bool RayCast(string direction,int rookX, int rookY, char[][] board){
+        
+        int scanX = rookX;
+        int scanY = rookY;
+        bool stopScan = false;
+        bool hit = false;
+        while (scanX >= 0 && scanX < 8 && scanY >= 0 && scanY < 8 && !stopScan)
         {
-            public int NumRookCaptures(char[][] board)
+            if (board[scanY][scanX] == 'B')
             {
-                int rookY = 0;
-                int rookX = 0;
-                int hits = 0;
-
-                for (int y = 0; y < board.Length; y++)
-                {
-                    for (int x = 0; x < board[y].Length; x++)
-                    {
-                        Console.Write(board[y][x]);
-                        if (board[y][x] == 'R')
-                        {
-                            rookX = x;
-                            rookY = y;
-                        }
-                    }
-                    Console.WriteLine();
-                }
-
-                int scanX = rookX;
-                int scanY = rookY;
-                bool stopScan = false;
-                while (scanX >= 0 && scanX < 8 && scanY >= 0 && scanY < 8 && !stopScan)
-                {
-                    Console.WriteLine("scanning: " + scanX + ", " + scanY);
-                    if (board[scanY][scanX] == 'B')
-                    {
-                        stopScan = true;
-                    }
-                    if (board[scanY][scanX] == 'p')
-                    {
-                        hits++;
-                        stopScan = true;
-                    }
+                stopScan = true;
+            }
+            if (board[scanY][scanX] == 'p')
+            {
+                hit = true;
+                stopScan = true;
+            }
+            switch(direction){
+                case "up":
                     scanY--;
-                }
-
-                scanX = rookX;
-                scanY = rookY;
-                stopScan = false;
-                while (scanX >= 0 && scanX < 8 && scanY >= 0 && scanY < 8 && !stopScan)
-                {
-                    Console.WriteLine("scanning: " + scanX + ", " + scanY);
-                    if (board[scanY][scanX] == 'B')
-                    {
-                        stopScan = true;
-                    }
-                    if (board[scanY][scanX] == 'p')
-                    {
-                        hits++;
-                        stopScan = true;
-                    }
+                    break;
+                case "down":
                     scanY++;
-                }
-
-                scanX = rookX;
-                scanY = rookY;
-                stopScan = false;
-                while (scanX >= 0 && scanX < 8 && scanY >= 0 && scanY < 8 && !stopScan)
-                {
-                    Console.WriteLine("scanning: " + scanX + ", " + scanY);
-                    if (board[scanY][scanX] == 'B')
-                    {
-                        stopScan = true;
-                    }
-                    if (board[scanY][scanX] == 'p')
-                    {
-                        hits++;
-                        stopScan = true;
-                    }
+                    break;
+                case "right":
                     scanX++;
-                }
-
-                scanX = rookX;
-                scanY = rookY;
-                stopScan = false;
-                while (scanX >= 0 && scanX < 8 && scanY >= 0 && scanY < 8 && !stopScan)
-                {
-                    Console.WriteLine("scanning: " + scanX + ", " + scanY);
-                    if (board[scanY][scanX] == 'B')
-                    {
-                        stopScan = true;
-                    }
-                    if (board[scanY][scanX] == 'p')
-                    {
-                        hits++;
-                        stopScan = true;
-                    }
+                    break;
+                case "left":
                     scanX--;
-                }
-
-
-                Console.WriteLine(hits);
-
-
-
-                return hits;
+                    break;
             }
         }
+        return hit;
+    }
+    
+    public int[] FindRookXY(char[][] board){
+        int rookX = 0;
+        int rookY = 0;
+        for(int y = 0; y < board.Length; y++){
+            for(int x = 0; x < board[y].Length; x++){
+                if(board[y][x] == 'R'){
+                    rookX = x;
+                    rookY = y;
+                }
+            }
+        }
+        return new int[]{rookX,rookY};
+    }
+    
+}
