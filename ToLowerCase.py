a5096396f03da90bf4b2ddedf6db3c870115d9ea
joaquin telleria <joaquintelleria@gmail.com>
2019-11-18 10:31:11 +0100

    Create ToLowerCase.py

---
+++ ToLowerCase.py
@@ -0,0 +1,8 @@
+/*
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+/*
+
+class Solution(object):
+    def toLowerCase(self, str):
+        return str.lower()
+        
