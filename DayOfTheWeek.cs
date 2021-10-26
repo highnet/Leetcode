ac232d72f447af34862d6c5c146f8a247e3faa05
joaquin telleria <joaquintelleria@gmail.com>
2021-10-26 20:24:54 +0200

    Update DayOfTheWeek.cs

--- DayOfTheWeek.cs
+++ DayOfTheWeek.cs
@@ -1,10 +1,3 @@
-/*
-Given a date, return the corresponding day of the week for that date.
-
-The input is given as three integers representing the day, month and year respectively.
-
-Return the answer as one of the following values {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}.
-*/
 public class Solution {
     public string DayOfTheWeek(int day, int month, int year) {
         
